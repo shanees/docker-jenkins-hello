c8cee82baba8f75f1a919abc6abede8c6bd9ecc8
shanees <shaneesmk4@gmail.com>
2020-05-18 17:31:38 +0530

    initial commit

---
+++ src/main/java/com/example/hello1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.hello1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello1")
+	public String test() {
+
+		return "hello 1";
+	}
+
+}
